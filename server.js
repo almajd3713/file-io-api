8aaf46ce6ca4a54343bb7d1a7212a02485f3b723
almajd3713 <almajd3713@gmail.com>
2024-06-24 14:36:38 +0400

    add simple server work + working upload

--- server.js
+++ server.js
@@ -1,4 +1,38 @@
+const path = require("path")
 const express = require("express")
-const multer = require("multer")
 const fs = require("fs")
-const uuid = require("uuid")
+const uuid = require("uuid").v4
+
+const multer = require("multer")
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    let uploadPath = "./data/"
+    console.log(file.mimetype)
+    if(file.mimetype.startsWith("image/"))
+      uploadPath += "images/"
+
+    fs.mkdirSync("uploadPath", {recursive: true})
+    cb(null, uploadPath)
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${uuid()}_${file.originalname}`)
+  }
+})
+const upload = multer({ dest: "data/", storage: storage})
+
+
+const PORT = 5656
+const app = express()
+app.use(express.static(__dirname + "/public"))
+
+
+
+app.get("/", (req, res) => {
+  res.end()
+})
+
+
+app.post("/post", upload.single("uploadedFile"), (req, res) => {
+})
+
+app.listen(PORT, () => console.log("Server has started!"))
